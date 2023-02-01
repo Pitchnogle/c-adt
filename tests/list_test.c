dcebec6a71e38708f091af00f27dbfc0470a3786
Justin Hadella <pitchnogle@gmail.com>
2023-01-31 19:28:22 -0700

    add more insertion tests

--- tests/list_test.c
+++ tests/list_test.c
@@ -50,6 +50,65 @@ Test(list_tests, insert_into_empty)
 	cr_expect(list_data(list_tail(&list)) == item1, "tail should be the item inserted");
 }
 
-// TODO -- insert after head
+Test(list_tests, insert_after_head)
+{
+	// Give out items some values
+	*item1 = 1;
+	*item2 = 2;
+
+	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
+	cr_expect(list_size(&list) == 1, "list's size should be 1");
+	cr_expect(list_insert_next(&list, list_head(&list), item2) == 0, "insert after head should return 0");
+	cr_expect(list_size(&list) == 2, "list's size should be 2");
+	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
+	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
+	cr_expect(list_next(list_head(&list)) != NULL, "next should be not NULL for head of two element list");
+	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of two element list");
+	cr_expect(list_data(list_head(&list)) == item1, "head should be the first item inserted");
+	cr_expect(list_data(list_tail(&list)) == item2, "tail should be the second item inserted");
+}
+
+Test(list_tests, insert_twice_at_head)
+{
+	// Give out items some values
+	*item1 = 1;
+	*item2 = 2;
+	*item3 = 3;
+
+	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
+	cr_expect(list_size(&list) == 1, "list's size should be 1");
+	cr_expect(list_insert_next(&list, NULL, item2) == 0, "insert after head should return 0");
+	cr_expect(list_size(&list) == 2, "list's size should be 2");
+	cr_expect(list_insert_next(&list, NULL, item3) == 0, "insert after head should return 0");
+	cr_expect(list_size(&list) == 3, "list's size should be 3");
+	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
+	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
+	cr_expect(list_next(list_head(&list)) != NULL, "next should be not NULL for head of two element list");
+	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of two element list");
+	cr_expect(list_data(list_head(&list)) == item3, "head should be the third item inserted");
+	cr_expect(list_data(list_tail(&list)) == item1, "tail should be the first item inserted");
+}
+
+Test(list_tests, insert_twice_at_tail)
+{
+	// Give out items some values
+	*item1 = 1;
+	*item2 = 2;
+	*item3 = 3;
+
+	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
+	cr_expect(list_size(&list) == 1, "list's size should be 1");
+	cr_expect(list_insert_next(&list, list_tail(&list), item2) == 0, "insert after tail should return 0");
+	cr_expect(list_size(&list) == 2, "list's size should be 2");
+	cr_expect(list_insert_next(&list, list_tail(&list), item3) == 0, "insert after tail should return 0");
+	cr_expect(list_size(&list) == 3, "list's size should be 3");
+	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
+	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
+	cr_expect(list_next(list_head(&list)) != NULL, "next should be not NULL for head of three element list");
+	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of three element list");
+	cr_expect(list_data(list_head(&list)) == item1, "head should be the first item inserted");
+	cr_expect(list_data(list_tail(&list)) == item3, "tail should be the third item inserted");
+}
+
 // TODO -- remove at head
 // TODO -- remove after head
