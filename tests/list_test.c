8640866752ffee56c523b862ae2620dc4b963bd2
Justin Hadella <pitchnogle@gmail.com>
2023-01-31 18:50:44 -0700

    add insert into empty test

--- tests/list_test.c
+++ tests/list_test.c
@@ -6,8 +6,16 @@
 
 List list;
 
+int *item1 = NULL;
+int *item2 = NULL;
+int *item3 = NULL;
+
 void suite_setup()
 {
+	item1 = (int*)malloc(sizeof (int));
+	item2 = (int*)malloc(sizeof (int));
+	item3 = (int*)malloc(sizeof (int));
+
 	list_init(&list, free);
 }
 
@@ -24,3 +32,24 @@ Test(list_tests, empty)
 	cr_expect(list_head(&list) == NULL, "empty list's head should be NULL");
 	cr_expect(list_tail(&list) == NULL, "empty list's tail should be NULL");
 }
+
+Test(list_tests, insert_into_empty)
+{
+	// Give our item a value
+	*item1 = 1;
+
+	cr_expect(list_insert_next(&list, NULL, item1) == 0, "insert into empty list should return 0");
+	cr_expect(list_size(&list) == 1, "list's size should be 1");
+	cr_expect(list_head(&list) != NULL, "list's head should be not NULL");
+	cr_expect(list_tail(&list) != NULL, "list's tail should be not NULL");
+	cr_expect(list_is_head(&list, list_head(&list)) == 1, "head should be at the head of the list");
+	cr_expect(list_is_tail(list_tail(&list)) == 1, "tail should be at the tail of the list");
+	cr_expect(list_next(list_head(&list)) == NULL, "next should be NULL for head of one element list");
+	cr_expect(list_next(list_tail(&list)) == NULL, "next should be NULL for tail of one element list");
+	cr_expect(list_data(list_head(&list)) == item1, "head should be the item inserted");
+	cr_expect(list_data(list_tail(&list)) == item1, "tail should be the item inserted");
+}
+
+// TODO -- insert after head
+// TODO -- remove at head
+// TODO -- remove after head
